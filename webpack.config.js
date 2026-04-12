e90f1a26ea22b6ee98edef6804cdb08bf8b75ced
DynoW <104089032+DynoW@users.noreply.github.com>
2026-04-12 14:19:47 +0300

    fix: fix webview vendor TS6059 compile error by excluding vendor dir for bundlers

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const config = {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /node_modules/,
+        exclude: /node_modules|vendor/,
         use: [
           {
             loader: "ts-loader",
