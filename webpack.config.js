f17abd1a56683b37814702207bae45f28c895517
DynoW <104089032+DynoW@users.noreply.github.com>
2026-09-19 14:19:17 +0300

    chore: bump to 1.2.0, simplify package scripts, clean dist output

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ const config = {
     path: path.resolve(__dirname, "dist"),
     filename: "extension.js",
     libraryTarget: "commonjs2",
+    clean: true,
     devtoolModuleFilenameTemplate: "../[resource-path]",
   },
   devtool: "source-map",
